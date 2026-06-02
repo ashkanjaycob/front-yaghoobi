603126086719d68693bf39b9847e603a9f512c25
ashkanjaycob <ashkan.yaghobii@gmail.com>
2026-06-03 01:49:28 +0400

    feat: add posts section with responsive grid layout and fetch functionality

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,15 +1,42 @@
+const grid = document.getElementById('posts-grid');
+
 const getPosts = async () => {
   try {
     const response = await fetch(
       'https://cloud.codesupply.co/endpoint/react/data.json'
     );
-    if (!response.ok) {
-      throw new Error('Could not fetch data Now !');
-    }
-    const data = await response.json();
-    console.log(data);
+    if (!response.ok) throw new Error('Could not fetch data');
+    const posts = await response.json();
+
+    if (!grid) return;
+
+    grid.innerHTML = posts
+      .map(
+        (post) => `
+      <article class="post-card">
+        <img
+          src="${post.img}"
+          srcset="${post.img} 1x, ${post.img_2x} 2x"
+          alt="${post.title}"
+          class="post-card-img"
+        />
+        <p class="post-card-category">${post.tags}</p>
+        <h2 class="post-card-title">${post.title}</h2>
+        <span class="flex"> 
+        <p class="post-card-author">${post.autor}</p>
+        <ol class="post-card-statics">
+          <li>${post.date}</li>
+          <li>${post.views} Views</li>
+        </ol>
+        </span>
+        <p class="post-card-text">${post.text}</p>
+      </article>
+    `
+      )
+      .join('');
   } catch (error) {
     console.error(error, 'There is an issue !');
   }
 };
+
 getPosts();
