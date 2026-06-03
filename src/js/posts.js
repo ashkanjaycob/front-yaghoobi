de3d7310e8c6506ee0dd56988b1cb25308e6b96e
ashkanjaycob <ashkan.yaghobii@gmail.com>
2026-06-03 12:26:39 +0400

    feat: implement real-time search functionality for blog posts

--- src/js/posts.js
+++ src/js/posts.js
@@ -2,7 +2,36 @@ import { openModal } from './modal.js';
 
 const grid = document.getElementById('posts-grid');
 
-let loadedPosts = [];
+export let loadedPosts = [];
+
+export function displayPosts(posts) {
+  if (!grid) return;
+
+  grid.innerHTML = posts
+    .map(
+      (post) => `
+    <article class="post-card" data-index="${loadedPosts.indexOf(post)}">
+      <img
+        src="${post.img}"
+        srcset="${post.img} 1x, ${post.img_2x} 2x"
+        alt="${post.title}"
+        class="post-card-img"
+      />
+      <p class="post-card-category">${post.tags}</p>
+      <h2 class="post-card-title">${post.title}</h2>
+      <span class="flex"> 
+      <p class="post-card-author">${post.autor}</p>
+      <ol class="post-card-statics">
+        <li>${post.date}</li>
+        <li>${post.views} Views</li>
+      </ol>
+      </span>
+      <p class="post-card-text">${post.text}</p>
+    </article>
+  `
+    )
+    .join('');
+}
 
 const getPosts = async () => {
   try {
@@ -13,32 +42,7 @@ const getPosts = async () => {
     const posts = await response.json();
     loadedPosts = posts;
 
-    if (!grid) return;
-
-    grid.innerHTML = posts
-      .map(
-        (post, index) => `
-      <article class="post-card" data-index="${index}">
-        <img
-          src="${post.img}"
-          srcset="${post.img} 1x, ${post.img_2x} 2x"
-          alt="${post.title}"
-          class="post-card-img"
-        />
-        <p class="post-card-category">${post.tags}</p>
-        <h2 class="post-card-title">${post.title}</h2>
-        <span class="flex"> 
-        <p class="post-card-author">${post.autor}</p>
-        <ol class="post-card-statics">
-          <li>${post.date}</li>
-          <li>${post.views} Views</li>
-        </ol>
-        </span>
-        <p class="post-card-text">${post.text}</p>
-      </article>
-    `
-      )
-      .join('');
+    displayPosts(posts);
   } catch (error) {
     console.error(error, 'There is an issue !');
   }
